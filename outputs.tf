ee6387eb3970a3e7c07ce6b69f21744cb4348dc7
Daniel Allen <dan@aeternagroup.net>
2026-01-11 15:31:30 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "iothub_device_update_instances" {
+  description = "All iothub_device_update_instance resources"
+  value       = azurerm_iothub_device_update_instance.iothub_device_update_instances
+}
+
---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "iothub_device_update_instances" {
+  description = <<EOT
+Map of iothub_device_update_instances, attributes below
+Required:
+    - device_update_account_id
+    - iothub_id
+    - name
+Optional:
+    - diagnostic_enabled
+    - tags
+    - diagnostic_storage_account (block):
+        - connection_string (required)
+        - id (required)
+EOT
+
+  type = map(object({
+    device_update_account_id = string
+    iothub_id                = string
+    name                     = string
+    diagnostic_enabled       = optional(bool, false)
+    tags                     = optional(map(string))
+    diagnostic_storage_account = optional(object({
+      connection_string = string
+      id                = string
+    }))
+  }))
+}
+
